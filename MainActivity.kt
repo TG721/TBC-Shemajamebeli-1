cb1404c4f0c7f7702cb20b8e1f7b07243ab73e60
TG721 <85778941+TG721@users.noreply.github.com>
2022-06-20 09:45:38 +0400

    2 functions of class are done

---
+++ MainActivity.kt
@@ -0,0 +1,38 @@
+class MathFunctions(){
+
+    fun  largestCommonDivider(x: Int, y: Int): Int  {
+        var i=1;
+        var lcd=1;
+        while (i<=x && i<=y) {
+            if(x % i == 0 && y % i ==0)
+            {
+                lcd = i
+
+            }
+            i++
+        }
+        return lcd
+    }
+
+    fun leastCommonMultiple(x: Int, y: Int): Int
+    {
+        
+        var lcm = if(x>y) x else y
+
+        while(true){
+            if (lcm % x == 0 && lcm % y == 0) {
+                break
+            }
+            lcm++
+        }
+        return lcm
+
+    }
+
+}
+
+
+fun main(){
+
+}
+
